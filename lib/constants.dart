a2bb712c61837bc38e5e0bf74932c72b2f14e19d
Giulio Pretis <giuliopretis@gmail.com>
2022-06-13 21:08:55 +0200

    added dio package basic get brands api endpoint

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+// ignore_for_file: constant_identifier_names
+
+const String PRODUCTION_URL = 'https://staging.orderpod.app/api/v3';
--- lib/data/api_provider.dart
+++ lib/data/api_provider.dart
@@ -1,1 +1,15 @@
-class ApiProvider {}
+import 'package:dio/dio.dart';
+import 'package:gestione_brand/constants.dart';
+
+class ApiProvider {
+  final Dio _dio = Dio();
+
+  Future<Response> getBrandList() async {
+    try {
+      Response res = await _dio.get('$PRODUCTION_URL/brands');
+      return res;
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
